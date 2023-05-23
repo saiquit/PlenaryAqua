1fd14232f364824f2f997fcaea7f6621442d9352
saiquit <imamhossain130754@gmail.com>
2023-05-23 13:02:24 +0600

    cart banner

--- resources/views/frontend/store/cart.blade.php
+++ resources/views/frontend/store/cart.blade.php
@@ -3,22 +3,23 @@
     Cart
 @endsection
 @section('main')
-    <section class="breadcrumb-section set-bg" data-setbg="/static/f/img/breadcrumb.jpg"
-        style="background-image: url(/static/f/img/breadcrumb.jpg);">
+    <!-- Breadcrumb Section Begin -->
+    <section class="breadcrumb-section set-bg" data-setbg="{{ asset('static/f/img/banner/banner-shop-green.jpg') }}">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <div class="breadcrumb__text">
-                        <h2>Shopping Cart</h2>
+                    <div class="breadcrumb__text black_heading_text">
+                        <h2>Cart</h2>
                         <div class="breadcrumb__option">
                             <a href="{{ route('front.home') }}">Home</a>
-                            <span>Shopping Cart</span>
+                            <span>Cart</span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+    <!-- Breadcrumb Section End -->
 
     <div id="loading" class=" position-fixed" style="left: 50%;">
         <div class="spinner-border" role="status">
