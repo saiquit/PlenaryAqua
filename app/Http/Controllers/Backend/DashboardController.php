a4bc7d4c5df568be488ec96f2e911d3102cf37d4
saiquit <imamhossain130754@gmail.com>
2023-09-13 17:52:10 +0600

    fix dash

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        // TODO: Dashboard page graphs 
+        // TODO: Dashboard page graphs
         $orders_per_month = Order::with('variations')->get()->groupBy(function ($val) {
             return Carbon::parse($val->created_at)->format('m');
         });
@@ -36,10 +36,12 @@ class DashboardController extends Controller
         $order_var = DB::table('order_variation')->get();
         foreach ($order_var as $key => $item) {
             // dump(Variation::find($item->variation_id)->name_en);
+            if (isset(Variation::find($item->variation_id)->product)) {
             if (isset($pi_data[Variation::find($item->variation_id)->product->name_en])) {
                 $pi_data[Variation::find($item->variation_id)->product->name_en] += 1;
             } else {
                 $pi_data[Variation::find($item->variation_id)->product->name_en] = 1;
+            }
             }
         }
         $orders = Order::where('shipping_status', '!=', 'delivered')->orWhere('status', '!=', 'complete')->latest()->get();
